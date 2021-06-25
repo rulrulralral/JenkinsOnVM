f00c9a1f1d0a0c89b9f1b6d7f6c50d0e5aa1f7dd
rulrulralral <86398821+rulrulralral@users.noreply.github.com>
2021-06-25 17:39:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I aaaaaaaaaaaa!');
+    res.send('I aaaaaaaaaadsasadsadaa!');
 });
 
 app.get('/start', function(req, res){
