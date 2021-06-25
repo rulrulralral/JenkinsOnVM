2ec20ce117b4e22ca822d5c7e12e8087a45c487c
rulrulralraler.com <dong45685@naver.com>
2021-06-25 16:11:58 +0900

    yyyyooo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Test 1!');
+    res.send('I gagag!');
 });
 
 app.get('/start', function(req, res){
