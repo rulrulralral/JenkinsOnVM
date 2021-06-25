619db60ded474294afdf662276134b99987dbd72
rulrulralraler.com <dong45685@naver.com>
2021-06-25 16:51:13 +0900

    yyyyooo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I gagadsdg!');
+    res.send('I gagadsdsdg!');
 });
 
 app.get('/start', function(req, res){
